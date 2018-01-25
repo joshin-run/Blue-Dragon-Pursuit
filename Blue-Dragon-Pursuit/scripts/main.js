f091017670a716bad87f22c72d62c05c01842cbf
Josh Holladay <josh@jcreateit.com>
2018-01-25 11:48:54 -0700

    Feat (style) add font assets, responsive design, button function

--- Blue-Dragon-Pursuit/scripts/main.js
+++ Blue-Dragon-Pursuit/scripts/main.js
@@ -1,18 +1,17 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Cmd-R),
- * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
- * 3. Display to insert the result in a comment after the selection. (Cmd-L)
- */
+function reset() {
+  location.reload();
+}
 
 function play() {
-  alert("You've searched high and low for the blue dragon. It is here, to the northern stretches of the world, that the great falcon has taken you. But to no avail. It seems as though the sea of stone will be your resting place.");
-  answer()
+  document.getElementById("banner-logo").style.display = "none";
+  document.getElementById("title").style.display = "none";
+  document.getElementById("play-button").style.display = "none";
+  // alert("You've searched high and low for the blue dragon. It is here, to the northern stretches of the world, that the great falcon has taken you. But to no avail. It seems as though the sea of stone will be your resting place.");
+  // answer()
 }
+
 function answer() {
-  var action = prompt("What are you going to do now? Lay down and die, search the sky above, or rummage in your pack for your final meal? (Type die, search, or meal.)");
+  let action = prompt("What are you going to do now? Lay down and die, search the sky above, or rummage in your pack for your final meal? (Type die, search, or meal.)");
   if (action === "die") {
     alert("Suddenly, as you were about to lay down and give in to the injustices of your fate, the great talons of the blue dragon dig into your shoulders as his massive arms launch you into the tangle of jagged rocks ahead. You have just enough time to look back and see the beauty of such a beast right before you realize you left your helmet at the castle. Game over!!!");
   } else if (action === "search") {
@@ -27,10 +26,10 @@ function answer() {
 function search() {
   document.getElementById("demo").innerHTML = prompt.search.jump;
   alert("You jump to your feet and tumble to a place behind a tall boulder jetting out of the earth ten feet away. Did he see you? You hope not.");
-  var weapon = prompt("As you crouch lower, you start to move toward your chosen weapon of attack. What whill you choose to fight the great blue dragon with? Your sword, bow and arrow, or the sharp rock you just found under your left foot? Choose sword, bow, or rock.");
+  let weapon = prompt("As you crouch lower, you start to move toward your chosen weapon of attack. What whill you choose to fight the great blue dragon with? Your sword, bow and arrow, or the sharp rock you just found under your left foot? Choose sword, bow, or rock.");
   if (weapon === "sword") {
     alert("You grab your" + " " + weapon + "just in time to send it deep into the chest of the dragon is it swoops down to attack you.");
-    var randomNumber = Math.round(Math.random()*15);
+    let randomNumber = Math.round(Math.random()*15);
     console.log(randomNumber);
     if (randomNumber <= 5) {
       alert("randomNumber <= 5");
---
+++ Blue-Dragon-Pursuit/scripts/time.js
@@ -0,0 +1,42 @@
+// function updateClock() {
+//    let now = new Date(), // current date
+//        months = ['January', 'February', '...']; // you get the idea
+//        time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
+//
+//        // a cleaner way than string concatenation
+//        date = [now.getDate(),
+//                months[now.getMonth()],
+//                now.getFullYear()].join(' ');
+//
+//    // set the content of the element with the ID time to the formatted string
+//    document.getElementById('time').innerHTML = [date, time].join(' / ');
+//
+//    // call this function again in 1000ms
+//    setTimeout(updateClock, 1000);
+// }
+// updateClock(); // initial call
+
+
+// function randomNumberFunc() {
+//   let randomNumber = Math.round(Math.random()*50);
+//   document.getElementById('time').innerHTML = randomNumber;
+//   // console.log(randomNumber);
+//   setTimeout(randomNumberFunc, 1000);
+// }
+// randomNumberFunc();
+// let random = "hello";
+// let random = Math.round(Math.random()*50);
+//    let now = new Date(), // current date
+//        months = ['January', 'February', '...']; // you get the idea
+//        time = now.getHours() + ':' + now.getMinutes(), // again, you get the idea
+//
+//        // a cleaner way than string concatenation
+//        date = [now.getDate(),
+//                months[now.getMonth()],
+//                now.getFullYear()].join(' ');
+//
+ // set the content of the element with the ID time to the formatted string
+
+//
+ // call this function again in 1000ms
+ // setTimeout(random, 1000);
